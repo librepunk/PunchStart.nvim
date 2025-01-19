b36cccdc7ec081b6d2673303eae4e9ee6191008b
Leila Draghi <leila.draghi@pledgestar.com>
2025-01-19 03:43:27 -0800

    meh

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,30 @@
+return {
+
+  'SirVer/ultisnips',
+  -- version = '*',
+  -- dependencies = {
+  --     '[git]/[repo]'
+  -- },
+  -- cmd = '',
+  -- keys = {
+  -- },
+  -- or
+  -- opts = {
+  --       map()
+  -- },
+
+  'NlGHT/vim-reasyntax',
+  -- version = '*',
+  -- dependencies = {
+  --     '[git]/[repo]'
+  -- },
+  -- cmd = '',
+  -- keys = {
+  -- },
+  -- or
+  -- opts = {
+  --       map()
+  -- },
+}
+
+-- vi:ft=lua
--- lua/plugins/supercollider.lua
+++ lua/plugins/supercollider.lua
@@ -1,13 +1,11 @@
 return {
+
   'davidgranstrom/scnvim',
   opts = {
     sclang = {
       cmd = 'C:/Program Files/SuperCollider-3.13.0/sclang.exe',
     },
   },
-  'madskjeldgaard/nvim-supercollider-piano',
-  'madskjeldgaard/fzf-sc',
-  'davidgranstrom/telescope-scdoc.nvim',
   -- version = '*',
   -- dependencies = {
   --     '[git]/[repo]'
@@ -19,6 +17,60 @@ return {
   -- opts = {
   --       map()
   -- },
-}
 
+  'madskjeldgaard/nvim-supercollider-piano',
+  opts = {
+    sclang = {
+      cmd = 'C:/Program Files/SuperCollider-3.13.0/sclang.exe',
+    },
+  },
+  -- version = '*',
+  -- dependencies = {
+  --     '[git]/[repo]'
+  -- },
+  -- cmd = '',
+  -- keys = {
+  -- },
+  -- or
+  -- opts = {
+  --       map()
+  -- },
+
+  'madskjeldgaard/fzf-sc',
+  opts = {
+    sclang = {
+      cmd = 'C:/Program Files/SuperCollider-3.13.0/sclang.exe',
+    },
+  },
+  -- version = '*',
+  -- dependencies = {
+  --     '[git]/[repo]'
+  -- },
+  -- cmd = '',
+  -- keys = {
+  -- },
+  -- or
+  -- opts = {
+  --       map()
+  -- },
+
+  'davidgranstrom/telescope-scdoc.nvim',
+  opts = {
+    sclang = {
+      cmd = 'C:/Program Files/SuperCollider-3.13.0/sclang.exe',
+    },
+  },
+  -- version = '*',
+  -- dependencies = {
+  --     '[git]/[repo]'
+  -- },
+  -- cmd = '',
+  -- keys = {
+  -- },
+  -- or
+  -- opts = {
+  --       map()
+  -- },
+ 
+ }
 -- vi:ft=lua
