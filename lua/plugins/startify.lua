39c0b4dcb6fd18b96ac4d977cec1d1ce2387df2a
librepunk <librepunk@gmail.com>
2025-01-19 03:41:24 -0800

    new plugins

---
+++ lua/plugins/startify.lua
@@ -0,0 +1,16 @@
+return {
+  'mhinz/vim-startify',
+  -- version = '*',
+  -- dependencies = {
+  --     '[git]/[repo]'
+  -- },
+  -- cmd = '',
+  -- keys = {
+  -- },
+  -- or
+  -- opts = {
+  --       map()
+  -- },
+}
+
+-- vi:ft=lua
